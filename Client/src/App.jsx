2eb35e6e401077d0299ff59eaa526e017acd6864
Priyanshu Das <daspriyanshu291@gmail.com>
2026-02-06 15:51:20 +0530

    first commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import HomePage from './pages/HomePage'
+import {Routes, Route} from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+
+const App = () => {
+  return (
+    <div className='bg-[url(./assets/chat-app-assets/bgImage.svg)] bg-contain'>
+      <Routes>
+        <Route path='/' element={<HomePage/>} /> 
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/profile' element={<ProfilePage/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
