510a230f1d582922376af1cc9ffb5aa2cc8adb5f
priyanshu <daspriyanshu291@gmail.com>
2026-03-06 22:58:22 +0530

    Fixed cors2

--- server/server.js
+++ server/server.js
@@ -104,10 +104,14 @@ app.use("/api/status", (req, res) => {
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
-await connectDB();
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
-});
+if (!process.env.VERCEL) {
+    server.listen(PORT, () => {
+        console.log(`Server is running on port http://localhost:${PORT}`);
+    });
+}
+
+export default app;
